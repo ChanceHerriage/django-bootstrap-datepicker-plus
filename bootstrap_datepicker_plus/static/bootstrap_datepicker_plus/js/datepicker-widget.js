6e025518bb336da724977a75f15e34d40d1b895e
Chance Herriage <chance.herriage@ttu.edu>
2020-06-30 23:15:51 -0500

    Updating js file to ignore $.fn.constructor
    Because we are not including bootstrap.min.js we are running into an error due to collapse never being added to the constructor. Ideally this would be  okay but you simply don't need it with the limited features we are running with this datepicker.

--- bootstrap_datepicker_plus/static/bootstrap_datepicker_plus/js/datepicker-widget.js
+++ bootstrap_datepicker_plus/static/bootstrap_datepicker_plus/js/datepicker-widget.js
@@ -1,10 +1,10 @@
-jQuery(function ($) {
+jQuery(function($) {
     var datepickerDict = {};
-    var isBootstrap4 = $.fn.collapse.Constructor.VERSION.split('.').shift() == "4";
+    var isBootstrap4 = true;
     function fixMonthEndDate(e, picker) {
         e.date && picker.val().length && picker.val(e.date.endOf('month').format('YYYY-MM-DD'));
     }
-    $("[dp_config]:not([disabled])").each(function (i, element) {
+    $("[dp_config]:not([disabled])").each(function(i, element) {
         var $element = $(element), data = {};
         try {
             data = JSON.parse($element.attr('dp_config'));
@@ -14,41 +14,41 @@ jQuery(function ($) {
             data.$element = $element.datetimepicker(data.options);
             data.datepickerdata = $element.data("DateTimePicker");
             datepickerDict[data.id] = data;
-            data.$element.next('.input-group-addon').on('click', function(){
+            data.$element.next('.input-group-addon').on('click', function() {
                 data.datepickerdata.show();
             });
-            if(isBootstrap4){
-                data.$element.on("dp.show", function (e) {
+            if (isBootstrap4) {
+                data.$element.on("dp.show", function(e) {
                     $('.collapse.in').addClass('show');
                 });
             }
         }
     });
-    $.each(datepickerDict, function (id, to_picker) {
+    $.each(datepickerDict, function(id, to_picker) {
         if (to_picker.linked_to) {
             var from_picker = datepickerDict[to_picker.linked_to];
             from_picker.datepickerdata.maxDate(to_picker.datepickerdata.date() || false);
             to_picker.datepickerdata.minDate(from_picker.datepickerdata.date() || false);
-            from_picker.$element.on("dp.change", function (e) {
+            from_picker.$element.on("dp.change", function(e) {
                 to_picker.datepickerdata.minDate(e.date || false);
             });
-            to_picker.$element.on("dp.change", function (e) {
+            to_picker.$element.on("dp.change", function(e) {
                 if (to_picker.picker_type == 'MONTH') fixMonthEndDate(e, to_picker.$element);
                 from_picker.datepickerdata.maxDate(e.date || false);
             });
             if (to_picker.picker_type == 'MONTH') {
-                to_picker.$element.on("dp.hide", function (e) {
+                to_picker.$element.on("dp.hide", function(e) {
                     fixMonthEndDate(e, to_picker.$element);
                 });
                 fixMonthEndDate({ date: to_picker.datepickerdata.date() }, to_picker.$element);
             }
         }
     });
-    if(isBootstrap4) {
-        $('body').on('show.bs.collapse','.bootstrap-datetimepicker-widget .collapse',function(e){
+    if (isBootstrap4) {
+        $('body').on('show.bs.collapse', '.bootstrap-datetimepicker-widget .collapse', function(e) {
             $(e.target).addClass('in');
         });
-        $('body').on('hidden.bs.collapse','.bootstrap-datetimepicker-widget .collapse',function(e){
+        $('body').on('hidden.bs.collapse', '.bootstrap-datetimepicker-widget .collapse', function(e) {
             $(e.target).removeClass('in');
         });
     }
